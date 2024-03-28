454e5fc83b6ec805f360ac010ef360f137530091
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-03-28 17:58:56 +0300

    Implementation of Figure Rotator

--- Assets/App/Scripts/Scenes/SceneMatrix/Features/FigureRotator/Services/FigureRotatorDummy.cs
+++ Assets/App/Scripts/Scenes/SceneMatrix/Features/FigureRotator/Services/FigureRotatorDummy.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using App.Scripts.Modules.Grid;
 using UnityEngine;
 
@@ -7,17 +9,55 @@ namespace App.Scripts.Scenes.SceneMatrix.Features.FigureRotator.Services
     {
         public Grid<bool> RotateFigure(Grid<bool> grid, int rotateCount)
         {
-            var rotated = new Grid<bool>(grid.Size);
+            int rotations = ((rotateCount % 4) + 4) % 4;
+            Dictionary<Vector2Int, bool> matrix = new();
 
-            for (int i = 0; i < grid.Height; i++)
+            foreach (var index in Sequence(Vector2Int.zero, grid.Size))
             {
-                for (int j = 0; j < grid.Width; j++)
+                var rotatedIndex = Rotate(index, rotations);
+                matrix.Add(rotatedIndex, grid[index]);
+            }
+
+            var newSize = Rotate(grid.Size, rotations);
+            newSize = AbsVector2Int(newSize);
+            var rotated = new Grid<bool>(newSize);
+
+            var orderedIndexes = matrix.Keys.OrderBy(v => v.x + v.y);
+            var shift = AbsVector2Int(orderedIndexes.First());
+            foreach (var index in orderedIndexes)
+            {
+                rotated[index + shift] = matrix[index];
+            }
+
+            return grid;
+        }
+
+        private IEnumerable<Vector2Int> Sequence(Vector2Int start, Vector2Int end)
+        {
+            for (int i = start.x; i < end.x; i++)
+            {
+                for (int j = start.y; j < end.y; j++)
                 {
-                    rotated[j, i] = Random.value > 0.5f;
+                    yield return new Vector2Int(i, j);
                 }
             }
-            
-            return rotated;
+        }
+
+        private Vector2Int Rotate(Vector2Int vector, int count)
+        {
+            return (count) switch
+            {
+                0 => vector,
+                1 => new Vector2Int(-vector.y, vector.x),
+                2 => new Vector2Int(-vector.x, -vector.y),
+                3 => new Vector2Int(vector.y, -vector.x),
+                _ => throw new System.ArgumentException()
+            };
+        }
+
+        private Vector2Int AbsVector2Int(Vector2Int vector)
+        {
+            return Vector2Int.Max(vector, vector * -1);
         }
     }
 }
